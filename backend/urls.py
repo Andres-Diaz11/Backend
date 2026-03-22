7394f0e1db7cdf6597b7d9307206a9756ee0b869
Andres-Diaz11 <andresfelipediazbarbosa1122@gmail.com>
2026-03-21 19:44:37 -0500

    Resaltamos para iniciar sesion

--- backend/urls.py
+++ backend/urls.py
@@ -10,6 +10,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_tareas.urls')),
+    # Endpoint para iniciar sesión (retorna access y refresh token)
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     # Generador del esquema YAML
